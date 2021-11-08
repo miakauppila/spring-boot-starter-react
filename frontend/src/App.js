26e1119d4a8d4e9ac8c76700b90aefcfa4958a95
miakauppila <mia.kauppila@gmail.com>
2021-11-09 00:05:51 +0200

    frontend created

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">Users List</h1>
+    </div>
+  );
+}
+
+export default App;
