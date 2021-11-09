757e6b52f0ba8f45afa804e8fa52ff14db3228d3
miakauppila <mia.kauppila@gmail.com>
2021-11-09 23:20:16 +0200

    add CrossOrigin annotation

--- backend/src/main/java/com/miakauppila/backend/controller/UserController.java
+++ backend/src/main/java/com/miakauppila/backend/controller/UserController.java
@@ -6,10 +6,12 @@ import com.miakauppila.backend.model.User;
 import com.miakauppila.backend.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("api/")
 public class UserController {
